72fab3b4b05fd86fb7b5e2c8bdbb7999c9489dab
Jimmy Lu <lujianming000@gmail.com>
2021-08-05 14:35:34 -0700

    homepage with css

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
+import '../public/css/style.css'
+
+function MyApp ({Component, pageProps}) {
+    return <Component {...pageProps}/>;
+}
+
+export default MyApp;
