e8aea24311afa5f6d960dc54fb25dca5128bbe2c
Jimmy Lu <lujianming000@gmail.com>
2021-08-05 15:13:58 -0700

    login register with routes and css

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 const Index  = () => {
     return (
         <>
-        <h1 className="jumbotron  text-center bg-primary">Hello World</h1>
+        <h1 className="jumbotron text-center bg-primary square">Logic Gate</h1>
         <p>text</p>
         </>
     );
---
+++ client/pages/register.js
@@ -0,0 +1,10 @@
+const Register = () => {
+    return (
+        <>
+        <h1 className="jumbotron text-center bg-primary square">Register</h1>
+        <p>Register page</p>
+        </>
+    );
+};
+
+export default Register;
