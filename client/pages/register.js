209061e2ecc7d1f046d56bb55391fa17b8519fe9
Jimmy Lu <lujianming000@gmail.com>
2021-08-05 15:36:38 -0700

    register part

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 const Register = () => {
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.table({name,email,password});
+    };
+
+
+
     return (
         <>
-        <h1 className="jumbotron text-center bg-primary square">Register</h1>
-        <p>Register page</p>
+            <h1 className="jumbotron text-center bg-primary square">Register</h1>
+            <div className="container col-md-4 offset-md-4 pb-5">
+                <form onSubmit={handleSubmit}>
+                    <input type="Name" className="form-control mb-4 p-4" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name" required></input>
+                    <input type="Email" className="form-control mb-4 p-4" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email" required></input>
+                    <input type="Password" className="form-control mb-4 p-4" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Password" required></input>  
+                    <button type="submit" className= "btn btn-block btn-primary">
+                        Submit
+                    </button>
+                </form>
+            </div>
         </>
     );
 };
